94396d164a998e577241ac0d62270d030072d8bf
Xueling22 <zuiainiwyw@163.com>
2024-03-15 04:58:44 +0000

    52

---
+++ 02-backend/spring-boot-eccommerce/src/main/java/com/luv2code/ecommerce/SpringBootEccommerceApplication.java
@@ -0,0 +1,12 @@
+package com.luv2code.ecommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootEccommerceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootEccommerceApplication.class, args);
+	}
+}
